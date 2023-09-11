10d5991ec0f0fad3fa94eb52374587386528b083
Highog92 <Madsserritslev@gmail.com>
2023-09-11 13:51:16 +0200

    Posters skelet

--- Wallywood/src/Pages/Posters.jsx
+++ Wallywood/src/Pages/Posters.jsx
@@ -1,8 +1,26 @@
 export function Posters() {
 
-    return (
-        <section>
+  return (
+    <section>
+      <section>
+        <div>
+          <img src="" alt="" />
+        </div>
 
+        <article>
+          <h3>Film overskrift</h3>
+          <p>Om filmen</p>
+          <p>Størrelse</p>
+          <p>Genre:</p>
+          <p>Pris</p>
+          <div>
+            <p><input type="number" />{}</p>
+            <button>Læg i kurv</button>
+            <p>Her skal der stå hvor mange der er på lager</p>
+          </div>
+        </article>
       </section>
-    )
+
+    </section>
+  )
 }
