33ee3339864fd8511486934c2abeda69e7096252
Highog92 <Madsserritslev@gmail.com>
2023-09-13 08:46:10 +0200

    update

--- Wallywood/src/Components/Navigation/Navigation.jsx
+++ Wallywood/src/Components/Navigation/Navigation.jsx
@@ -6,14 +6,13 @@ export const Navigation = () => {
 
     return (
         <nav className={style.navStyle}>
-            <div>WALLYWOOD</div>
+            <h1 className={style.logo}>WALLYWOOD</h1>
             <div>
                 <Link to="/">HOME</Link>
                 <Link to="/posters">PLAKATER</Link>
                 <Link to="/about">OM OS</Link>
                 <Link to="/contact">KONTAKT</Link>
                 <Link to="/login">LOGIN</Link>
-
             </div>
         </nav>
     )
--- Wallywood/src/Pages/Home.jsx
+++ Wallywood/src/Pages/Home.jsx
@@ -2,18 +2,7 @@ import { useEffect, useState } from "react"
 import { MovieCard } from "../Components/Card/MovieCard"
 
 export function Home() {
-  // const [userData, setUserData] = useState([])
-  // useEffect(() => {
 
-  //   const fetchUsers = () => {
-
-  //     const url = 'http://localhost:4000/poster/list'
-  //     fetch(url).then(result => result.json()).then(data => setUserData(data))
-  //   }
-  //   fetchUsers()
-  // }, [])
-
-  // console.log("data", userData);
 
   return (
     <section>
