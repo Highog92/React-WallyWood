828d9e6e703db82e4793e915f3427db02c53d3be
Highog92 <Madsserritslev@gmail.com>
2023-09-15 12:24:33 +0200

    Login og signup

--- Wallywood/src/Components/Login/Signup.jsx
+++ Wallywood/src/Components/Login/Signup.jsx
@@ -4,27 +4,58 @@ import { useForm } from 'react-hook-form'
 
 export function Signup() {
 
+
+
   const { register, handleSubmit, formState: { errors } } = useForm()
 
   const onSubmit = (data) => {
     console.log(data);
+
+    const url = 'http://localhost:4000/users'
+    const body = new URLSearchParams()
+    body.append('firstname', data.firstname)
+    body.append('lastname', data.lastname)
+    body.append('email', data.email)
+    body.append('password', data.password)
+    body.append('org_id', '1')
+    body.append('is_active','1')
+
+    
+
+    const options = {
+      method: 'POST',
+      body: body
+    }
+
+    fetch(url, options).then(res => res.json()).then(data=> console.log(data));
+
   }
-
-  // className={style.contactStyle}
   return (
+    // className={style.contactStyle}
     <section  >
-      <h2>Login</h2>
+      <h2>Eller opret bruger</h2>
+
       <form onSubmit={handleSubmit(onSubmit)} action="">
-        <label htmlFor="">Brugernavn
-          <input {...register('username', { required: true, minLength: 4 })} type="username" />
-          {errors.username == 'required' && <span>Du skal udfylde Brugernavn</span>}
-          {errors.username == 'minLength' && <span>Du skal have et brugernavn længere end 4 bogstaver</span>}
-        </label>
-        <label htmlFor="login">Password
-        <input {...register('password', { required: true })} type="username" />
-        {errors.password == 'required' && <span>Du skal udfylde Password</span>}
+        <label htmlFor="">Fornavn
+          <input {...register('firstname', { required: true, })} type="username" />
+          {errors.firstname?.type === 'required' && <span>Du skal udfylde Efternavn</span>}
         </label>
 
+        <label htmlFor="">Efternavn
+          <input {...register('lastname', { required: true })} type="username" />
+          {errors.lastname?.type === 'required' && <span>Du skal udfylde Efternavn</span>}
+        </label>
+
+        <label htmlFor="">Email
+          <input {...register('email', { required: true })} type="username" />
+          {errors.email?.type === 'required' && <span>Du skal udfylde Email</span>}
+        </label>
+
+
+        <label htmlFor="login">Password
+          <input {...register('password', { required: true })} type="password" />
+          {errors.password?.type === 'required' && <span>Du skal udfylde Password</span>}
+        </label>
 
         <input type="submit" value="submit" />
       </form>
--- Wallywood/src/Pages/Login.jsx
+++ Wallywood/src/Pages/Login.jsx
@@ -1,5 +1,7 @@
 // import style from '../Layout/MainLayout.module.scss'
 import { useForm } from 'react-hook-form'
+import { Signup } from '../Components/Login/Signup';
+import { useState } from 'react';
 
 
 export function Login() {
@@ -7,27 +9,53 @@ export function Login() {
   const { register, handleSubmit, formState: { errors } } = useForm()
 
   const onSubmit = (data) => {
-    console.log(data);
+
+    const url = 'http://localhost:4000/login'
+    const body = new URLSearchParams()
+    body.append('username', data.username)
+    body.append('password', data.password)
+
+    const options = {
+      method: 'POST',
+      body: body
+    }
+
+    fetch(url, options).then(res => res.json()).then(data => console.log(data));
+
   }
 
-  // className={style.contactStyle}
-  return (
-    <section  >
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)} action="">
-        <label htmlFor="">Brugernavn
-          <input {...register('username', { required: true, minLength: 4 })} type="username" />
-          {errors.username == 'required' && <span>Du skal udfylde Brugernavn</span>}
-          {errors.username == 'minLength' && <span>Du skal have et brugernavn længere end 4 bogstaver</span>}
-        </label>
-        <label htmlFor="login">Password
-        <input {...register('password', { required: true })} type="username" />
-        {errors.password == 'required' && <span>Du skal udfylde Password</span>}
-        </label>
+  const [isLoginTrue, setIsLoginTrue] = useState(true)
 
+  if (isLoginTrue)
+    return (
+      <section>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)} action="">
+          <label htmlFor="">Brugernavn
+            <input {...register('username', { required: true, minLength: 4 })} type="username" />
+            {errors.username?.type === 'required' && <span>Du skal udfylde Brugernavn</span>}
+            {errors.username?.type === 'minLength' && <span>Du skal have et brugernavn længere end 4 bogstaver</span>}
+          </label>
+          <label htmlFor="login">Password
+            <input {...register('password', { required: true })} type="password" />
+            {errors.password == 'required' && <span>Du skal udfylde Password</span>}
+          </label>
 
-        <input type="submit" value="submit" />
-      </form>
-    </section>
-  )
+          <input type="submit" value="submit" />
+        </form>
+
+        <button onClick={() => setIsLoginTrue(false)}>Eller opret ny bruger</button>
+      </section>
+    )
+
+  else
+    return (
+      <section  >
+
+        <Signup />
+        <button onClick={() => setIsLoginTrue(true)}>Har du allerede en bruger? Login her</button>
+
+      </section>
+    )
 }
+// className={style.contactStyle}
